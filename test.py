17f3b2fd66fdf2e0b15be2fae4085187d5facf3a
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2023-10-11 21:58:50 +0200

    solved with few methods

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+from datetime import date, datetime
 
-print(numbers[::-2])
+def print_birthday_info(user):
+    if 'birthday' in user:
+        birthday = user['birthday']
+        formatted_birthday = birthday.strftime('%Y-%m-%d')  # Форматуємо дату
+        print(f"{user['name']}'s birthday: {formatted_birthday}")
+    else:
+        print(f"No birthday information found for {user['name']}")
+
+# Приклад використання
+user = {"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()}
+print_birthday_info(user)
--- working_with_files/files_7_r_w.py
+++ working_with_files/files_7_r_w.py
@@ -6,17 +6,47 @@
 запишите новое очищенное от цифр содержимое файла output, используя менеджер контекста with и режим "w"
 запись нового содержимого файла output должна быть единоразовая и использовать метод write"""
 
-def sanitize_file(source, output):
-    try:
-        with open(source, 'r') as source_file, open(output, 'w') as output_file:
-            for line in source_file:
-                sanitized_line = ''.join(char for char in line if not char.isdigit())
-                output_file.write(sanitized_line)
-        print(f'Файл {source} був очищений від цифр і збережений у файл {output}')
-    except FileNotFoundError:
-        print(f'Помилка: файл {source} не знайдено.')
+# def sanitize_file(source, output):
+#     try:
+#         with open(source, 'r') as source_file, open(output, 'w') as output_file:
+#             for line in source_file:
+#                 sanitized_line = ''.join(char for char in line if not char.isdigit())
+#                 output_file.write(sanitized_line)
+#         print(f'Файл {source} був очищений від цифр і збережений у файл {output}')
+#     except FileNotFoundError:
+#         print(f'Помилка: файл {source} не знайдено.')
         
-source_file = "/home/sergio/Desktop/my python tasks/working_with_files/ingredients.txt"
+# source_file = "/home/sergio/Desktop/my python tasks/working_with_files/ingredients.txt"
 
-output_file = '/home/sergio/Desktop/my python tasks/working_with_files/output.txt' 
-sanitize_file(source_file, output_file)
+# output_file = '/home/sergio/Desktop/my python tasks/working_with_files/output.txt' 
+# sanitize_file(source_file, output_file)
+
+
+# def sanitize_file(source, output):
+#     with open(source, 'r') as file_1, open(output, 'w') as file_2:
+#         for line in file_1:
+#             line_1 = ''
+#             for char in line:
+#                 if not char.isdigit():
+#                     line_1 += char
+#             file_2.write(line_1)
+# source_file = "/home/sergio/Desktop/my python tasks/working_with_files/ingredients.txt"
+
+# output_file = '/home/sergio/Desktop/my python tasks/working_with_files/output.txt' 
+# sanitize_file(source_file, output_file)
+
+# def sanitize_file(source, output):
+#     with open(source, 'r') as file_1, open(output, 'w') as file_2:
+#         for line in file_1:
+#             sanitized_line = line.replace('0', '').replace('1', '').replace('2', '').replace('3', '').replace('4', '').replace('5', '').replace('6', '').replace('7', '').replace('8', '').replace('9', '')
+#             file_2.write(sanitized_line)
+            
+def sanitize_file(source, output):
+    with open(source, 'r') as file_1, open(output, 'w') as file_2:
+        for line in file_1:
+            chars_to_remove = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+            sanitized_line = line
+            for char in chars_to_remove:
+                sanitized_line = sanitized_line.replace(char, '')
+            file_2.write(sanitized_line)
+            
