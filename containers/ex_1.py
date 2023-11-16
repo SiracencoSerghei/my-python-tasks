aa2c7c7f73b2ea22723e949b2c81f15a6aec5e1e
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2023-11-16 21:04:22 +0100

    new examples

---
+++ containers/ex_1.py
@@ -0,0 +1,29 @@
+from collections import UserList
+from random import randint
+
+
+class MyList(UserList):
+    info = 'This is my list class'
+
+    def get_positive(self):
+        return list(filter(lambda x: x > 0, self.data))
+
+    def get_negative(self):
+        return list(filter(lambda x: x < 0, self.data))
+
+    def get_info(self):
+        return self.info
+
+
+r = []
+for _ in range(0, 35):
+    r.append(randint(-10, 10))
+print(r)
+result = MyList(r)
+print(result)
+#
+result.append(5555)
+print(result)
+print(result.get_positive())
+# print(result.get_negative())
+# print(result.get_info())
