2292ecbf726e18016a80bcef9f82fd3c372dc17d
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2023-12-05 13:10:07 +0100

    add one example

--- first_practice/strings.py
+++ first_practice/strings.py
@@ -33,3 +33,36 @@ info2 = "%s likes %s at %s o'clock" % (name, hobby, time)
 
 print(info)
 print(info2)
+
+
+"""Move the first letter of each word to the 
+end of it, then add "ay" to the end of the 
+word. Leave punctuation marks untouched.
+
+
+"""
+
+# def pig_it(text):
+#     punctuation =".\"',;:/"
+#     words = text.split()
+#     for i in range(len(words)):
+#         if not words[i].isalpha():
+#             continue
+#         if words[i].endswith(tuple(punctuation)):
+#             words[i] = words[i][1:-1] + words[i][0] + 'ay' +words[i][-1]
+#         else:
+#             words[i] = words[i][1:] +words[i][0] + 'ay'
+#     return " ".join(words)
+
+from string import punctuation
+def pig_it(text):
+    words = text.split(' ')
+    return ' '.join(
+        [
+            '{}{}ay'.format(
+                word[1:],
+                word[0]
+            ) if word not in punctuation else word for word in words
+        ]
+    )
+    
