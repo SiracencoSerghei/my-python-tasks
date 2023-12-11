16a34cc0982f80596d2d83d793cc6a02e13cb19f
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2023-12-11 23:20:33 +0100

    add task with regex

--- codewars/rgb_to_hex.py
+++ codewars/rgb_to_hex.py
@@ -1,4 +1,3 @@
-
 """
 
 The rgb function is incomplete. 
@@ -19,16 +18,22 @@ Examples (input --> output):
 """
 
 
+# def rgb(r, g, b):
+#     def clamp(value):
+#         return min(255, max(0, value))
+#
+#     r = clamp(r)
+#     g = clamp(g)
+#     b = clamp(b)
+#
+#     return f"{r:02X}{g:02X}{b:02X}"
+
 def rgb(r, g, b):
-    def clamp(value):
-        return min(255, max(0, value))
-
-    r = clamp(r)
-    g = clamp(g)
-    b = clamp(b)
-
-    return f"{r:02X}{g:02X}{b:02X}"
+    return ('{0:02X}{1:02X}{2:02X}'
+            .format(max(min(r, 255), 0),
+                    max(min(g, 255), 0),
+                    max(min(b, 255), 0)))
     
-    
+
 result = rgb(255, 255, 255)
 print(result)
